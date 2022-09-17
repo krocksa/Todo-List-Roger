54db3a694dd847459128b714e1e61ac4fa0f1e9e
krocksa <rogeryonatan@gmail.com>
2022-09-17 00:30:16 +0000

    Medio Proyecto Listerine

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [ inputValue, setInputValue ] = useState('');
+	const [ all, setAll ] = useState([]);
+
+	function handleChange(parameter) {
+		const value = parameter.target.value;
+		setInputValue(value);
+	}
+
+	function handleAdd(parameter) {
+		let copyAll = [...all];
+		copyAll.push(inputValue);
+		setAll(copyAll);		
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todolist">
+			<div className="dentroDe">
+				<h1>ALL TASKS</h1>
+			<div className="input">
+				<input type="text" placeholder="Input Task" value={inputValue} onChange={handleChange} />
+				<button onClick={handleAdd}>Add</button>
+			</div>
+				{
+					all.map((one,index) => {
+						return(
+							<ListGroup>
+							<ListGroup.Item className="newOne" key={index}>
+								{one}
+							</ListGroup.Item>
+							</ListGroup>
+						)
+					})
+				}
+			</div>
 		</div>
 	);
 };
